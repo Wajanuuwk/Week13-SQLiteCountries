5211e465878d511e5883e06208a5f262358783cd
Opilane TTHK <opilane@tthk.ee>
2022-02-18 10:52:43 +0200

    mnigga

---
+++ SQLiteCountries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+
+        private static void ReadCountries()
+        {
+            Database databaseObj = new Database();
+            string query = "SELECT Country.countryName AS Country, Capital.capitalName AS Capital From Capital JOIN Country ON Capital.countryid = Country.rowid";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObj.myConnection);
+            databaseObj.OpenConnection();
+
+            SQLiteDataReader data = myCommand.ExecuteReader();
+
+            if (data.HasRows)
+            {
+                while(data.Read())
+                {
+                    Console.WriteLine($"Country: {data["Country"]}. Capital: {data["Capital"]}");
+                }
+            }
+
+            databaseObj.CloseConnection();
+
+        }
+    }
+}
